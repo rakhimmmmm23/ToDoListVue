5c0cbe6984cd439720fb96c259233a4db1892b41
rakhimfarkhodov <84928945+rakhimmmmm23@users.noreply.github.com>
2021-08-24 22:40:35 +0500

    Fixed Bugs New interface and functions

---
+++ app.vue
@@ -0,0 +1,33 @@
+const app ={
+  el:'#app',
+data(){
+    return{
+      beggin:'Введите список дел',
+      title:'To Do List',
+      inpvalue:'',
+      notes:[],
+    allert:'Нажмите на Задание чтобы его удалить или на кнопку',
+
+    }
+},
+methods:{
+  InputChangeHandler(event){
+this.inpvalue = event.target.value
+  },
+  addnewnote(){
+    if (this.inpvalue !== ''){
+       this.notes.push(this.inpvalue)
+    this.inpvalue = ''
+
+    }
+   
+  },
+  
+  removenote(idx){
+this.notes.splice(idx,1)
+  },
+  
+}
+}
+
+Vue.createApp(app).mount('#app')
